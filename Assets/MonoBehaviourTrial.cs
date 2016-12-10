7bd08da4af3b8bf19f2b326d2a9402e3d83e798f
tayutaedomo <tayutaedomo@gmail.com>
2016-12-10 17:09:26 +0900

    Add project files

---
+++ Assets/MonoBehaviourTrial.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonoBehaviourTrial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("On Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
